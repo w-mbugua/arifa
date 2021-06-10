fb9211d91d6537d8a1f4a7452a222719df933c64
JoyMbugua <wacumbugua@gmail.com>
2021-06-10 18:47:00 +0300

    added test for market model

--- markets/tests.py
+++ markets/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Market
 
 # Create your tests here.
+class MarketModelTests(TestCase):
+    def setUp(self):
+        self.new_market = Market(name='stocks', slug='stocks')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_market, Market)) 
