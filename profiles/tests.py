4b0014da0fae56d0c8f425415df2be82c9aa6442
JoyMbugua <wacumbugua@gmail.com>
2021-06-10 20:02:33 +0300

    added test for message model

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Profile
+from .models import Profile, Message, Reply, Review, Client
 from django.contrib.auth import get_user_model
 
 
@@ -15,3 +15,13 @@ class ProfileModelTest(TestCase):
     
     def test_string_method(self):
         self.assertEqual(str(self.new_profile), self.user1.username)
+
+class MessageModelTest(TestCase):
+    def setUp(self):
+        self.user1 =  get_user_model().objects.create_user(username = 'testuser', email = 'test@email.com', password='secret')
+        self.new_profile = Profile(user = self.user1, bio='here to test')
+        self.new_msg = Message(to=self.new_profile, f_rom=self.user1, subject='Nothing', message="get back to me asap")
+    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_msg, Message))
+
